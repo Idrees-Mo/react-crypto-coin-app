2a5d8f7d65327a5533f6ce0c093f4d6800e49711
Muhammad Idrees <idris@y7mail.com>
2023-08-31 20:31:59 +1000

    error page styles update

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,26 @@
 import { Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+// Styles for the container
+const useStyles = makeStyles((theme) => ({
+  container: {
+    minHeight: `calc(100vh - 194px)`,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
+
 function Error() {
+  const classes = useStyles();
+
   return (
     <>
       <Header />
-      <Container>
+      <Container className={classes.container}>
         <h1>Opps!</h1>
         <p>Page Not Found!</p>
       </Container>
