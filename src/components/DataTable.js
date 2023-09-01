0585c96e6a71f560d4e42f880c6c82d3ae8e3b21
Muhammad Idrees <idris@y7mail.com>
2023-09-01 13:31:49 +1000

    add reusable dataTable with dummy data

---
+++ src/components/DataTable.js
@@ -0,0 +1,80 @@
+// DataTable.js
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Pagination,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  tableHeader: {
+    fontWeight: "bold",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+  },
+  clickableRow: {
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}));
+
+function DataTable({ columns, data, onRowClick, itemsPerPage = 5 }) {
+  const classes = useStyles();
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedData = data.slice(startIndex, endIndex);
+
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell key={column.field} className={classes.tableHeader}>
+                  {column.headerName}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedData.map((row) => (
+              <TableRow
+                key={row.id}
+                className={`${classes.clickableRow} ${
+                  onRowClick ? classes.clickableRow : ""
+                }`}
+                onClick={() => onRowClick && onRowClick(row.id)}
+              >
+                {columns.map((column) => (
+                  <TableCell key={column.field}>{row[column.field]}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        count={Math.ceil(data.length / itemsPerPage)}
+        page={page}
+        onChange={handleChangePage}
+      />
+    </div>
+  );
+}
+
+export default DataTable;
--- src/pages/Coins.js
+++ src/pages/Coins.js
@@ -1,7 +1,40 @@
 import { Container } from "@mui/material";
+import DataTable from "../components/DataTable";
+
+const columns = [
+  { field: "id", headerName: "ID" },
+  { field: "name", headerName: "Name" },
+  { field: "age", headerName: "Age" },
+  { field: "hight", headerName: "Hight" },
+  // Add more columns as needed
+];
+
+const generateData = (count) =>
+  Array.from({ length: count }, (_, index) => ({
+    id: index + 1,
+    hight: `7 cm`,
+    name: `Person ${index + 1}`,
+    age: Math.floor(Math.random() * 50) + 20,
+  }));
+
+const data = generateData(50);
 
 function Coins() {
-  return <Container>Coins Page</Container>;
+  const handleRowClick = (id) => {
+    console.log(`Row with ID ${id} clicked`);
+  };
+
+  return (
+    <Container>
+      <h1>Coins Page</h1>
+      <DataTable
+        columns={columns}
+        data={data}
+        onRowClick={handleRowClick}
+        itemsPerPage={10}
+      />
+    </Container>
+  );
 }
 
 export default Coins;
