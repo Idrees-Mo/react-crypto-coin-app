e6d02bdbae48806e22b3588709ef7e6fa28ddd6f
Muhammad Idrees <idris@y7mail.com>
2023-09-03 19:04:53 +1000

    changes

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,5 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "../constants";
-let BASE_URL = process.env.REACT_APP_API_URL;
+import { BASE_URL } from "../constants";
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
