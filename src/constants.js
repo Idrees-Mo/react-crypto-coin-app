770526b9aace237aad67970c4493c8c172749288
Muhammad Idrees <idris@y7mail.com>
2023-09-04 19:43:59 +1000

    removed proxy

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = ""; // empty as using proxy
+export const BASE_URL = "https://api.coingecko.com/";
 export const COINS_URL = "api/v3/coins/";
--- src/pages/CoinDetail.js
+++ src/pages/CoinDetail.js
@@ -1,12 +1,16 @@
 import { useParams } from "react-router-dom";
-// import { useGetCoinDetailsQuery } from "../store/coinsApiSlice";
+import { useGetCoinDetailsQuery } from "../store/coinsApiSlice";
 
 function CoinDetail() {
   const { coinId } = useParams();
 
   // TODO the query is not creating corret URL
-  // const { data, loading, error } = useGetCoinDetailsQuery(coinId);
-
+  const { data, loading, error } = useGetCoinDetailsQuery(coinId);
+  console.warn({
+    data,
+    loading,
+    error,
+  });
   return (
     <div>
       <h1>Coins Details Page</h1>
